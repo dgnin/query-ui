b02e33810d92ee36a2e2262482f622c3278ca64b
David <me@davidgnin.com>
2017-02-10 18:46:17 +0100

    Updated tasks and demo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,9 @@ import browserify from 'browserify';
 import fs from 'fs';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import clean from 'gulp-clean';
+import runSequence from 'run-sequence';
 
 gulp.task('jshint', function () {
   return gulp.src('src/*.js')
@@ -19,16 +22,34 @@ gulp.task('jscs', function () {
     .pipe(jscs.reporter());
 });
 
-gulp.task('build', ['jshint', 'jscs'], function () {
+gulp.task('build-1', ['jshint', 'jscs'], function () {
   return browserify('src/index.js')
     .transform('babelify', { presets: ['es2015']})
     .bundle()
-    .pipe(fs.createWriteStream('build/query-ui.js'));
+    .pipe(fs.createWriteStream('build/query-ui-tmp.js'));
 });
 
-gulp.task('build-min', ['build'], function () {
+gulp.task('build-2', function () {
+  return gulp.src(['lib/*.js', 'build/query-ui-tmp.js'])
+    .pipe(concat('query-ui.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build-3', function () {
+  return gulp.src('build/query-ui-tmp.js').pipe(clean());
+});
+
+gulp.task('build', function () {
+  return runSequence('build-1', 'build-2', 'build-3');
+});
+
+gulp.task('build-4', function () {
   return gulp.src('build/query-ui.js')
     .pipe(uglify())
     .pipe(rename('query-ui-min.js'))
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('build-min', function () {
+  return runSequence('build-1', 'build-2', 'build-3', 'build-4');
 });
